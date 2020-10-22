8eaf02000bb0220b288f6c175631bcc56301353b
dv6230 <dveasia558@gmail.com>
2020-10-22 22:46:46 +0800

    Update create_account.php

--- create_account.php
+++ create_account.php
@@ -1,5 +1,32 @@
 <?php
 
+$name_error = false;
+
+if (isset($_POST['acnt']) && isset($_POST['pwd'])) {
+
+    require_once 'mydatabase.php';
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    $sql = "SELECT account FROM user WHERE account = $user_name";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $name_error = true;
+    }
+}
+
+
+
+function adduser($conn, $user_account, $user_password)
+{
+    $sql = "INSERT INTO user (account,password) VALUES ($user_account,$user_password)";
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+}
+
+
 ?>
 
 <!doctype html>
@@ -15,18 +42,27 @@
 
     <title>註冊帳號</title>
     <style>
+        body {
+
+            background: rgb(174, 238, 208);
+            background: radial-gradient(circle, rgba(174, 238, 208, 0.8354692218684349) 0%, rgba(73, 119, 255, 0) 100%);
+
+        }
+
         .b {
             height: 100vh;
         }
 
-        
-        .bg-7 {
+
+        .panel-shadow {
+            border: none;
             box-shadow: 5px 10px 18px #888888;
         }
-        
+
 
         img {
             width: 100%;
+            height: 100%;
             margin-left: auto;
             margin-right: auto;
         }
@@ -37,26 +73,38 @@
 
     <div class="container b">
         <div class="row d-flex align-items-center h-100">
-            <div class="border row bg-7">
-                <div class="col-md-7">
-                    <img src="img/p1.jpg" alt="">
+            <div class="row panel-shadow bg-light p-0">
+                <div class="col-md-7 m-0 p-0">
+                    <img src="img/p1.jpg" alt="" class="m-0 p-0">
                 </div>
-                <div class="col-md-5">
-                    <form class="d-flex flex-column">
+                <div class="col-md-5 ">
+                    <form class="d-flex flex-column pl-3 pr-3 mt-2 ">
+                        <div>
+                            <a href="javascript:history.back()" class="float-right m-2 d-inline">返回</a>
+                        </div>
+
                         <div class="col">
                             <div class="form-group">
                                 <label for="exampleInput1">帳號</label>
-                                <input type="text" class="form-control" name="acnt" id="exampleInput1" placeholder="Account">
+                                <input type="text" class="form-control" name="acnt" id="exampleInput1" placeholder="Account" required>
                             </div>
                             <div class="form-group">
                                 <label for="exampleInput2">密碼</label>
-                                <input type="password" class="form-control" name="pwd" id="exampleInput12" placeholder="Password">
+                                <input type="password" class="form-control" name="pwd" id="exampleInput12" placeholder="Password" required>
                             </div>
                             <div class="form-group">
-                                <label for="exampleInput3">確認密碼</label>
-                                <input type="password" class="form-control" id="exampleInput13" placeholder="Password">
+                                <label for="exampleInput3">再次輸入密碼</label>
+                                <input type="password" class="form-control" id="exampleInput13" placeholder="Password" required>
                             </div>
                         </div>
+
+                        <?php
+                        if ($name_error) {
+                            echo '<p class="text-danger">此帳號已被使用</p>';
+                        } else {
+                            echo '<br>';
+                        }
+                        ?>
                         <button type="submit" class="btn btn-primary w-50 m-auto">註冊</button>
 
 
