f864160abb4490b5711a6e424ef71e7f9628b65f
dv6230 <dveasia558@gmail.com>
2020-10-23 15:17:28 +0800

    Update create_success.php

---
+++ create_success.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <title>註冊成功</title>
+    <style>
+        body {
+            background: rgb(33, 24, 185);
+            background: linear-gradient(90deg, rgba(33, 24, 185, 0.8438725832129726) 0%, rgba(0, 212, 255, 1) 100%);
+        }
+
+        .bg-costom {
+            background: rgb(42, 244, 14);
+            background: linear-gradient(90deg, rgba(42, 244, 14, 0.7010154403558299) 0%, rgba(0, 255, 149, 0.45171572046787467) 100%);
+        }
+
+        .card-h {
+            height: 240px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="card p-0 m-0 border-0" style="width: 18rem;">
+            <h2 class="text-center mt-5">註冊成功</h2>
+            <div class="card-img-top m-0 card-h bg-primary d-flex justify-content-center align-items-center">
+
+                <img src="img/check.png" class="" width="96px" height="96px" alt="...">
+
+            </div>
+            <div class="card-body">
+                <a href="#" class="btn btn-primary w-100 mt-3">前往登入</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
