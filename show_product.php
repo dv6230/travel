39c863514eec820d623cda535762e557dd10bfd4
dv6230 <67547654+dv6230@users.noreply.github.com>
2020-10-23 22:34:59 +0800

    Update show_product.php

--- show_product.php
+++ show_product.php
@@ -16,9 +16,7 @@
 </head>
 
 <body>
-    <?php
-    session_start();
-    include 'index_navbar.php'; ?>
+    <?php include 'index_navbar.php'; ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
