f89f5e27cac3517aa86149c59f315d95b765f4b4
dv6230 <67547654+dv6230@users.noreply.github.com>
2020-10-24 22:32:43 +0800

    Update path_classify.php

--- tools/path_classify.php
+++ tools/path_classify.php
@@ -7,11 +7,11 @@ class backstage
 
     function __construct()
     {
-        $bkary[] = array('center', 'profile/profile_center.php', '0');
-        $bkary[] = array('order', 'profile/profile_order.php', '0');
-        $bkary[] = array('history', 'profile/profile_history.php', '0');
-        $bkary[] = array('else', 'profile/profile_else.php', '1');
-        $bkary[] = array('person', 'profile/profile_person.php', '0');
+        $this->bkary[] = array('center', 'profile/profile_center.php', '0');
+        $this->bkary[] = array('order', 'profile/profile_order.php', '0');
+        $this->bkary[] = array('history', 'profile/profile_history.php', '0');
+        $this->bkary[] = array('else', 'profile/profile_else.php', '1');
+        $this->bkary[] = array('person', 'profile/profile_person.php', '0');
     }
 
     function getbackstage($type, $auth)
@@ -19,8 +19,7 @@ class backstage
         $go_web = 'profile/profile_center.php';
         //(重要) 類別內的方法，如果要取用 CLASS 內的 參數 ， 需要用 $this->參數名稱
         foreach ($this->bkary as $value) {
-
-            if ($type == 'else' && $auth < 1) break;
+            if ($type === 'else' && $auth < 1) break;
             if (in_array($type, $value)) {
                 $go_web = $value[1];
             }
