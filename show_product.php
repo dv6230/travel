7364ac8adc6feff5b1674c5a7ad266721ecdcf07
dv6230 <67547654+dv6230@users.noreply.github.com>
2020-10-24 17:14:25 +0800

    Update show_product.php

--- show_product.php
+++ show_product.php
@@ -12,11 +12,30 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style1.css">
+    <link rel="stylesheet" href="css/style3.css">
     <title>產品列表</title>
 </head>
 
 <body>
     <?php include 'index_navbar.php'; ?>
+    <img src="img/show_product_1.jpg" alt="" class="head_img">
+    <div class="container mb-5">
+        <div class="p-5 m-2">
+            <h1>產品列表</h1>
+        </div>
+        <div class='row'>
+            <div class="describe col-md-5">
+                <h2>阿勒格尼國家森林</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro sequi molestiae quo alias harum! Reprehenderit, nobis dolor atque, quos, vel modi inventore voluptatum repellat est facilis nihil sequi quam consequatur.</p>
+                
+                <button class="btn btn-outline-primary d-flex align-items-end">1234</button>
+                
+            </div>
+            <img class="col-md-7 m-0 p-0" height="360px" src="https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80" alt="">
+        </div>
+    </div>
+
+    <?php include 'index_footer.html'; ?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
