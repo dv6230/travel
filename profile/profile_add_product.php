ea98c8a14c83bb0d1d9911a2bd535ef163d9fd8c
dv6230 <67547654+dv6230@users.noreply.github.com>
2020-10-26 00:51:23 +0800

    檔案上傳

--- profile/profile_add_product.php
+++ profile/profile_add_product.php
@@ -13,46 +13,28 @@ if (!($auth > 0)) {
 }
 */
 include '../mydatabase.php';
+require '../tools/manager_file_upload.php' ;
 
 $err = '';
 
 $title = '';
 $content = '';
 
+
 if (isset($_POST['title']) && isset($_POST['content']) && $_FILES) {
 
-
+    $process_file = new manager_file_upload();
+    
     $title = $_POST['title'];
     $content = $_POST['content'];
 
-    $file_name = $_FILES['image']['name'];
-    $file_random_name = uniqid();
-    switch ($_FILES['image']['type']) {
-        case 'image/jpeg':
-            $file_type = 'jpg';
-            break;
-        case 'image/png':
-            $file_type = 'png';
-            break;
-        case 'image/gif':
-            $file_type = 'gif';
-            break;
-        case 'image/webp':
-            $file_type = 'webp';
-            break;
-        default:
-            $file_type = '';
-            break;
-    }
-    if ($file_type) {
-        $file_path = '../product_image/' . $file_random_name . '.' . $file_type;
-        move_uploaded_file($_FILES['image']['tmp_name'], $file_path);
-    } else {
-        $err = '無法上傳此類型的檔案';
-    }
+    $err = $process_file->process_file($_FILES);
+
+    
     //header('Location: profile_add_product.php');
     $title = '';
     $content = '';
+   
 }
 
 //處理文字
--- tools/manager_file_upload.php
+++ tools/manager_file_upload.php
@@ -1,9 +1,35 @@
 <?php
 
-class file_upload
+class manager_file_upload
 {
-    public function FunctionName()
+    public function process_file($getfile)
     {
-    
+        $file_name = $_FILES['image']['name'];
+        $file_random_name = uniqid();
+        switch ($_FILES['image']['type']) {
+            case 'image/jpeg':
+                $file_type = 'jpg';
+                break;
+            case 'image/png':
+                $file_type = 'png';
+                break;
+            case 'image/gif':
+                $file_type = 'gif';
+                break;
+            case 'image/webp':
+                $file_type = 'webp';
+                break;
+            default:
+                $file_type = '';
+                break;
+        }
+        if ($file_type) {
+            $file_path = '../product_image/' . $file_random_name . '.' . $file_type;
+            move_uploaded_file($_FILES['image']['tmp_name'], $file_path);
+            $err = '成功上傳';
+        } else {
+            $err = '無法上傳此類型的檔案';
+        }
+        return $err ;
     }
 }
