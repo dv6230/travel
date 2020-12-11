3bdd3ae1ec4bb5cd787872d1497e766ed7dabb9f
dv6230 <dveasia558@gmail.com>
2020-12-11 16:58:16 +0800

    Update login.php

--- login.php
+++ login.php
@@ -13,21 +13,20 @@ if (isset($_POST['acnt']) && isset($_POST['pwd'])) {
 
     if ($result->num_rows > 0) {
         // output data of each row
-        while ($row = $result->fetch_assoc()) {
-            if ($row["password"] == $pwd) {
-                echo '成功登入';
-                $_SESSION['user_id'] = $row['id'];
-                $_SESSION['auth'] = $row['auth'];
+        $row = $result->fetch_assoc();
+        if ($row["password"] == $pwd) {
+            echo '成功登入';
+            $_SESSION['user_id'] = $row['id'];
+            $_SESSION['auth'] = $row['auth'];
 
-                if (empty($_POST['from'])) {
-                    header('Location:login_page.php');
-                } else {
-                    echo "<script>location.href='".$_POST['from']."';</script>";;
-                }
-            } else {
-                $_SESSION['wrong'] = '帳號或密碼輸入錯誤';
+            if (empty($_POST['from'])) {
                 header('Location:login_page.php');
+            } else {
+                echo "<script>location.href='" . $_POST['from'] . "';</script>";
             }
+        } else {
+            $_SESSION['wrong'] = '帳號或密碼輸入錯誤';
+            header('Location:login_page.php');
         }
     } else {
         $_SESSION['wrong'] = "帳號或密碼輸入錯誤";
